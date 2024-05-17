b7a3af7accffb1c2c870e83df831c3639a8a9196
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-05-17 19:15:51 -0300

    fixing the routes, post and get method

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,24 @@
 var userModel = require('../models/userModel');
 
+// function login(req,res){
+//     var email = req.body.email;
+//     var senha = req.body.senha;
+    
+//     userModel.autenticarLogin(email,senha).then((resultadoQuery)=>{
+//             console.log(resultadoQuery.success);
+//             if(resultadoQuery.success){
+//                 req.session.authenticated = true;
+//                 req.session.userId = resultadoQuery.userId
+//                 res.redirect('/dashboard', { userId: req.session.userId })
+//             }else{
+//                 res.render('login', {error : 'acessa certo mano'})
+//             }
+
+//         }
+//     );
+    
+// }
+
 function login(req,res){
     var email = req.body.email;
     var senha = req.body.senha;
@@ -7,18 +26,15 @@ function login(req,res){
     userModel.autenticarLogin(email,senha).then((resultadoQuery)=>{
             console.log(resultadoQuery.success);
             if(resultadoQuery.success){
-                req.session.authenticated = true;
-                req.session.userId = resultadoQuery.userId
-                res.redirect(`/dashboard?userId=${resultadoQuery.userId}`);
+                console.log('logado com sucesso')
             }else{
-                res.render('login', {error : 'acessa certo mano'})
+                res.render('login', {error : 'Usuário ou senha inválida. Tente novamente!'})
             }
 
         }
     );
     
 }
-
 module.exports = {
     login
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,14 +7,9 @@ router.get('/', function(req, res) {
     res.render('login', { error: null });
 });
 
-router.post('/login', function(req,res){
-    if (req.method === 'POST') {
-        // A solicitação é do tipo POST, então você pode continuar com o processamento
-        userController.login(req,res);
-    } else {
-        // Se a solicitação não for do tipo POST, envie uma resposta de erro
-        res.redirect('/');
-    }
+router.post('/', function(req,res){
+    // A solicitação é do tipo POST, então você pode continuar com o processamento
+    userController.login(req,res);
 
 });
 
